bf97d8ef5dcfbc2d834f57aeb3d2152c8bb67331
= <help.studentgrade@gmail.com>
2022-07-24 23:46:53 +0600

    register app inventory

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from inventory.models import Inventory
 # Register your models here.
+
+
+class InventoryAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'cost_per_item', 'stock_date', 'last_sales_date']
+    list_editable = ['cost_per_item']
+
+    class Meta:
+        model = Inventory
+
+
+admin.site.register(Inventory, InventoryAdmin)
+
