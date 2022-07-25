a3bc296033f1b10262b6ad208d04e20762b27b3c
= <help.studentgrade@gmail.com>
2022-07-25 23:44:12 +0600

    added bootstrap5

--- inventory/views.py
+++ inventory/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "inventory/index.html")
+    context = {
+        "title": "Home"
+    }
+    return render(request, "inventory/index.html", context)
