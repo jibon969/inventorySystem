6c0c39c826e8358effb958a24e5b80b90f5d0dfb
= <help.studentgrade@gmail.com>
2022-07-26 23:45:44 +0600

    added per-product

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Inventory
 # Create your views here.
 
@@ -10,3 +10,11 @@ def index(request):
         "inventories": inventories,
     }
     return render(request, "inventory/inventory.html", context)
+
+
+def per_product(request, pk):
+    inventories = get_object_or_404(Inventory, pk=pk)
+    context = {
+        "inventory": inventories,
+    }
+    return render(request, "inventory/per_product.html", context)
