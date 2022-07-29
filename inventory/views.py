1a6d36c9b99deab6ce2d2ec4421684ef7c286143
= <help.studentgrade@gmail.com>
2022-07-29 22:44:36 +0600

    add inventoty form

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Inventory
+from .forms import InventoryForm
 
 
 def index(request):
@@ -17,3 +18,16 @@ def per_product(request, pk):
         "inventory": inventories,
     }
     return render(request, "inventory/per_product.html", context)
+
+
+def add_inventory(request):
+    if request.method == "POST":
+        form = InventoryForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            form = InventoryForm()
+        context = {
+            'form': form
+        }
+        return render(request, "inventory/add_inventory.html", context)
